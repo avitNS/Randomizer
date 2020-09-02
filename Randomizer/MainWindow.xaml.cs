a7de6d80a2996cb31e4312a41718ff674ad37952
avitNS <avitnik@gmail.com>
2020-09-02 20:35:45 +0300

    Добавьте файлы проекта.

---
+++ Randomizer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace Randomizer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string Generate(Params myparams)
+        {
+            string result;
+            int temp;
+            Random random = new Random();
+            temp = random.Next(myparams.From, myparams.To);
+            result = Convert.ToString(temp);
+            return result;
+        }
+
+        private void btGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Params myparams = new Params();
+                myparams.GetValues(this);
+                string res = Generate(myparams);
+                tbNumber.Text = res;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show(@"Change values! From can not be more than To!");
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ Randomizer/Params.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    class Params
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public Params() { }
+        public void GetValues(MainWindow mainWindow)
+        {
+            if (mainWindow.tbFrom.Text == "" || mainWindow.tbFrom.Text==" ")
+            {
+                mainWindow.tbFrom.Text = "0";
+                From = 0;
+            }  
+            else
+                From = int.Parse(mainWindow.tbFrom.Text);
+            if (mainWindow.tbTo.Text == "" || mainWindow.tbTo.Text == " ")
+            {
+                mainWindow.tbTo.Text = "1";
+                To = int.Parse(mainWindow.tbTo.Text);
+            }
+            else
+                To = int.Parse(mainWindow.tbTo.Text);
+
+
+        }
+    }
+}
